a4d7f00e178686f697c163ed62a709428ad1195c
tobias monzon <tobiasmonzon.2@gmail.com>
2023-08-14 11:44:49 -0300

    cambios

--- ejercicio13.php
+++ ejercicio13.php
@@ -25,25 +25,3 @@ if($_POST){
 }
 ?>
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>switch</title>
-</head>
-<body>
-    <form action="ejercicio13.php" method="post">
-
-    <input type="submit" name="btnValor" value="1">
-    <br/>
-    <input type="submit" name="btnValor" value="2">
-    <br/>
-    <input type="submit" name="btnValor" value="3">
-    <br/>
-
-    </form>
-</body>
-</html>
